9bdec27f09bbb76dfca58d2e6482054f0f074de2
Harpreet Ghotra <harpreetghotra40@icloud.com>
2019-07-14 13:36:20 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 // Write your code in this file!
+var currentUser = "Harpreet";
+var welcomeMessage = 'Welcome to Flatbook, '+ currentUser + '!';
+var excitedWelcomeMessage = "WELCOME TO FLATBOOK, " + currentUser.toUpperCase() + "!";
+var shortGreeting = "Welcome, "+ currentUser[0] + "!";
